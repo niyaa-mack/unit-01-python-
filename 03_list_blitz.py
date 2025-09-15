afa59e5f8f49c6a4f53e5325898cf83c550df28f
Shaniya M <smack@brooklynsteamcenter.org>
2025-09-15 10:33:03 -0400

    done

---
+++ 03_list_blitz.py
@@ -0,0 +1,65 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+lis = [False, "hello", 12, "people"]
+print(lis)
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+li = [True, 31]
+li.append("Eyes")
+print(li)
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+l = ["mango", True, 0.2]
+l.remove("mango")
+print(l)
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+f = [False, "mango", 'pineapples']
+f[2] = "$"
+print(f)
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+s = [True, False, 'grapes']
+s.append ('mango')
+s.append('yes')
+s.append("people")
+print (s)
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+e = ["shaniya", "kael", "chris"]
+del e[1]
+print (e)
+"""
+Task 7: Slicing lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+t = [True, "niya", "peeps"]
+a = t[-1]
+print(a)
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
+a_1 = ["list", True]
+b_2 = ["set" , False]
+c = a_1 + b_2
+print (c)
