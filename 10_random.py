3c4347d63632ef4356bdb1622fe18fdd7c39c021
Shaniya M <smack@brooklynsteamcenter.org>
2025-10-09 09:14:59 -0400

    comments

--- 10_random.py
+++ 10_random.py
@@ -4,9 +4,9 @@ Task 1 (random module):
 Write a program that simulates rolling a six-sided die 10 times.
 Print each roll result.
 """
-for i in range(10):
-    dice = random.randrange(1,7)
-    print("Roll:", {dice})
+for i in range(10): #make the loop go 10 times
+    dice = random.randrange(1,7) #make a variable for the dice and print it 
+    print("Roll:", {dice}) 
 
     
 
@@ -16,13 +16,13 @@ Write a program that generates 5 random floating-point numbers between 0 and 1.
 Then generate 5 random floating-point numbers between 10 and 20.
 Print both sets of numbers.
 """
-for i in range(5):
-    float = random.randint(0,1)
-    print(float)
+for i in range(5): #loop for 5 times
+    float = random.randint(0,1) 
+    print(float) #print random number 0 - 1 fives time 
 else:
-    for i in range(5):
+    for i in range(5): #loop for 5 times
         roll_t = random.randrange(10,21)
-        print(roll_t)
+        print(roll_t) #print random numbers 10-20 5 times 
 
 
 """
@@ -30,16 +30,14 @@ Task 3 (random module):
 Create a list of colors: ["red", "blue", "green", "yellow", "purple"].
 Write a program that randomly selects and prints 3 colors from this list without replacement.
 """
-my_list = ["red", "blue", "green", "yellow", "purple"]
-by = random.sample(my_list, 3)
-for color in by:
-    print(f"COLOR: {by}")
-
+my_list = ["red", "blue", "green", "yellow", "purple"] #make a list 
+for i in range(3): 
+    print(random.choice(my_list)) #print a color from the list 3 times
 """
 Task 4 (random module):
 Write a program that creates a list of numbers from 1 to 10, then shuffles the list
 and prints the shuffled result.
 """
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-rand = random.randint(1, 10)
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #made a list 
+rand = random.randint(1, 10) #variable for randomizing the number 1 - 10 and printing the result
 print(rand)
