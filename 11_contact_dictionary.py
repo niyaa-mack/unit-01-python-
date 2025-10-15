13cab9f872ea252997fe5a9bbecd130312aa7981
Shaniya M <smack@brooklynsteamcenter.org>
2025-10-15 10:57:09 -0400

    comments (no extensions)

--- 11_contact_dictionary.py
+++ 11_contact_dictionary.py
@@ -8,33 +8,33 @@ while True:
     print("3. View the list")
     print("4. Exit")
     print()
-    ans = int(input("Enter your choice:"))
+    ans = int(input("Enter your choice:")) #pick the choice
     print()
 
-    if ans == 1:
-        name = input("What is the name?")
-        numb = int(input("what is the number of the person? no spaces"))
-        Contact[name] = numb
+    if ans == 1: #adding contacts
+        name = input("What is the name?") 
+        numb = int(input("what is the number of the person? no spaces")) #asking for their name and number 
+        Contact[name] = numb #adds it to the dictionary
         print("The contact is saved!")
        
      
     if ans == 2:
-        dele = input("Enter the name of the person you want to delete:")
+        dele = input("Enter the name of the person you want to delete:") #asks what person do they want to delete 
         if dele in Contact:
-            del Contact[dele]
+            del Contact[dele] #delete them from the dictionary 
             print(f'Contact "{name}" deleted successfully.')
         else: 
-            print(f'Contact "{name}" not found')
+            print(f'Contact "{name}" not found') #if they are not in the dictionary then it will say this 
 
     if ans == 3:
         if Contact:
             print("-----Contact Book--------")
-            for x in Contact:
+            for x in Contact: # lists out the contact lists
                 print(x)
                 print(Contact[x])
 
     if ans == 4:
-        break
+        break #exit the book
     
     
 
